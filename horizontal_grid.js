0ff8430151207b6ab6ac6692aa43607bd67ec30f
Quantbo <quantbo@quantbo.com>
2017-07-23 16:44:52 -0700

    Pulled the formation of a horizontal grid into a separate file: horizontal_grid.js

---
+++ horizontal_grid.js
@@ -0,0 +1,41 @@
+//Generate a horizontal grid.
+'use strict';
+
+//Set horizontal grid locations.
+//It improves clarity for the observer if there is a tick mark and tick label at the beginning and end of the time series.
+let hLocs = [0];
+const hGran = 16; //Granularity of horizontal grid.
+const hStep = (vbWidth - 1) / hGran;
+for (let ii = 1; ii <= hGran; ++ii) {
+  hLocs.push(Math.round(ii * hStep));
+}
+
+function hGrid(ii) {
+  //ii: Index of governing for-loop.
+  let loc = hLocs[ii];
+  let tick = document.createElementNS(namespaceURI, 'line');
+  tick.setAttribute('x1', loc);
+  tick.setAttribute('x2', loc);
+  tick.setAttribute('y1', vbHeight);
+  tick.setAttribute('y2', vbHeight * 1.03); //Overflow to create a tick mark.
+  tick.setAttribute('class', 'tick');
+  inner.appendChild(tick);
+  let gridLine = document.createElementNS(namespaceURI, 'line');
+  gridLine.setAttribute('x1', loc);
+  gridLine.setAttribute('x2', loc);
+  gridLine.setAttribute('y1', 0);
+  gridLine.setAttribute('y2', vbHeight);
+  gridLine.setAttribute('class', 'gridLine');
+  inner.appendChild(gridLine);
+  let text = document.createElementNS(namespaceURI, 'text');
+  text.setAttribute('x', loc);
+  const vbh_factor = 1.05;
+  text.setAttribute('y', vbHeight * vbh_factor);
+  text.setAttribute('class', 'tick-text');
+  text.setAttribute('transform', `rotate(-45, ${loc}, ${vbHeight * vbh_factor})`)
+  text.textContent = date[loc];
+  inner.appendChild(text);
+}
+for (let ii = 0; ii <= hGran; ++ii) {
+  hGrid(ii);
+}
